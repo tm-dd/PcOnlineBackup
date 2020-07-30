d6b0c34adf41c5c1b7184c4c7d7fd00e72fb2e6d
Thomas Mueller <developer@mueller-dresden.de>
2020-07-31 00:40:44 +0200

    some fixes

--- mount_backup_sshfs.sh
+++ mount_backup_sshfs.sh
@@ -22,12 +22,14 @@ sftpBackupTargetServer='login@server.domain.tld:'
 sftpBackupLocalMountPoint='/system_backups/system'
 encfsPassword='ENCFS PASSWORD'
 mailNotificationAddress='example@mail.org'
+encfsBackupData='/system_backups/system/encfsBackupData'
+encfsBackupMount='/system_backups/system/encfsBackupMount'
 
 # if nessesary, mount the sshfs target
 if [ -z "`/bin/mount | grep \"^$sftpBackupTargetServer on \"`" ]
 then
     /usr/bin/sshfs -o nonempty ${sftpBackupTargetServer} ${sftpBackupLocalMountPoint}
-    sleep 10
+    (set -x; sleep 5)
 fi
 
 # try the mount point
@@ -35,7 +37,7 @@ if [ -z "`/bin/mount | grep \"^$sftpBackupTargetServer on \"`" ]
 then
 
     # send a error message and stop the backup process, now
-    (date; echo; echo "ERROR: The backup target $sftpBackupLocalMountPoint count not mounted from sftpBackupTargetServer with sshfs."; echo; df -h | grep -v '^tmpfs ') | /usr/bin/mail -s "Error on server `hostname`" $mailNotificationAddress
+    (date; echo; echo "ERROR: The backup target $sftpBackupLocalMountPoint could not mounted from sftpBackupTargetServer with sshfs."; echo; df -h | grep -v '^tmpfs ') | /usr/bin/mail -s "Error on server `hostname`" $mailNotificationAddress
     exit -1
 
 fi
@@ -43,16 +45,24 @@ fi
 # if desired, mount the backup volumen with encfs to encrypt the backup on the target
 if [ -n "${encfsPassword}" ] && [ -z "`/bin/mount | grep \"/encfsBackupMount\"`" ]
 then
-    
-    mkdir -p /system_backups/system/encfsBackupData /system_backups/system/encfsBackupMount
-    ENCFSMESSAGE=`echo "${encfsPassword}" | encfs -i 10 -S /system_backups/system/encfsBackupData /system_backups/system/encfsBackupMount`
-    sleep 10
+
+    if [ -d "$encfsBackupData" ] && [ -d "$encfsBackupMount" ]
+    then
+        ENCFSMESSAGE=`echo "${encfsPassword}" | encfs -i 10 -S $encfsBackupData $encfsBackupMount`
+    else
+	mkdir -p $encfsBackupData $encfsBackupMount
+	echo -e "\nPlease setup encfs (and use the password '$encfsPassword' ), now.\n"
+        encfs -i 10 $encfsBackupData $encfsBackupMount
+    fi
+
+    (set -x; sleep 10)
     
     # possible error, stop the backup process now and send a mail
     if [ -n "${ENCFSMESSAGE}" ]
     then
+	echo "${ENCFSMESSAGE}" 
         echo "${ENCFSMESSAGE}" | mail -s "ENCFS and BACKUP ERROR on $HOSTNAME (BACKUP STOP NOW.)" ${mailNotificationAddress}
-        umount /system_backups/system/encfsBackupMount
+        umount $encfsBackupMount
         umount "${sftpBackupLocalMountPoint}"
         exit -1
     fi
